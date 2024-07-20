cf931762fc193ef1fff07f4ff30ff5b50857645e
UtkuCoskun <m.utkucoskun@icloud.com>
2024-07-21 00:04:08 +0300

    Initial Commit

---
+++ Medy/MedyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedyApp.swift
+//  Medy
+//
+//  Created by Utku on 21/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MedyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
